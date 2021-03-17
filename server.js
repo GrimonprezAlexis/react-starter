a15c6a1e6bb2346741b503709163fb46619eae88
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-03-17 01:18:19 +0100

    Start with heroku

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 require('dotenv').config(); // loads environment variables from a .env file into process.env
 
-const express = require('express'); // ExperssJS Framework
-const app = express(); // Invoke express to variable for use in application
-const path = require('path');
+const express = require('express');
+const app = express();
 
 const router = express.Router(); // Invoke the Express Router
 const apiRouter = require('./app/routes/router')(router); // Import the application end points/API
 app.use('/api', apiRouter); // Assign name to end points
 
-if(process.env.NODE_ENV == 'production'){
-    app.use(express.static(path.resolve(__dirname, 'client/build')));
-    app.get('*', function(req, res){
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-    });
-}
-
-const port = process.env.PORT || 5000; // Set default port or assign a port in enviornment
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// serve up production assets
+app.use(express.static('client/build'));
+// let the react app to handle any unknown routes 
+// serve up the index.html if express does'nt recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// if not in production use the port 5000
+const PORT = process.env.PORT || 5000;
+console.log('server started on port:',PORT);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
