63c5366cbd10ec13b68b8085c4d839f58a8b620d
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-03-17 01:13:32 +0100

    Start width heroku

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require('dotenv').config(); // loads environment variables from a .env file into
 
 const express = require('express'); // ExperssJS Framework
 const app = express(); // Invoke express to variable for use in application
+const path = require('path');
 
 const router = express.Router(); // Invoke the Express Router
 const apiRouter = require('./app/routes/router')(router); // Import the application end points/API
 app.use('/api', apiRouter); // Assign name to end points
+
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static(path.resolve(__dirname, 'client/build')));
+    app.get('*', function(req, res){
+        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000; // Set default port or assign a port in enviornment
 app.listen(port, () => console.log(`Server started on port ${port}`));
